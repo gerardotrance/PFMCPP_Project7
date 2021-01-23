ff1ab89a699340fc4cb1c086a7514fd698ece30d
gerardotrance <gezcoulter247@gmail.com>
2021-01-23 02:52:02 +0000

    ready for diffchecker

--- AttackItem.cpp
+++ AttackItem.cpp
@@ -1,1 +1,13 @@
 #include "AttackItem.h"
+#include "AttackItem.h"
+#include "Dwarf.h"
+#include "Paladin.h"
+#include "DragonSlayer.h"
+#include "Dragon.h"
+
+void AttackItem::use(Character* character)
+{
+    //assert(false); 
+    //make your defensive item use the appropriate Utility helper functions
+    useAttackItem(character, *this);
+}
--- AttackItem.h
+++ AttackItem.h
@@ -1,7 +1,8 @@
 #include "Item.h"
+#include "Character.h"
 
 struct AttackItem : Item
 {
     AttackItem() : Item("AttackItem", 10){}
-    
+    void use(Character* character) override;
 };
